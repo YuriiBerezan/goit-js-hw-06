99b82a2f26e73d0215c714d58c2877413b1c9165
Yurii Berezan <67425022+YuriiBerezan@users.noreply.github.com>
2022-05-02 17:17:16 +0300

    add 2 task

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,24 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulEl = document.querySelector('#ingredients');
+// console.log(ulEl)
+
+const ulAddLi = document.createElement('li');
+
+ulAddLi.textContent = 'onion';
+ulAddLi.classList.add('item')
+// console.log(ulAddLi)
+
+// const list = ingredients.map(option => {
+
+// })
+
+const list = ingredients.reduce((str, item) => str + `<li>${item}</li>`, '');
+// console.log(list)
+ulEl.innerHTML = list;
+// console.log(list)
+console.log(ulEl)
+// ulEl.append(list, ulAddLi);   
+ulEl.appendChild(ulAddLi)
