4c2676aa3e836cd2d439cf282ee9bb1f3ad125ff
Yurii Berezan <67425022+YuriiBerezan@users.noreply.github.com>
2022-05-09 14:27:04 +0300

    add task 5

---
+++ js/task-05.js
@@ -0,0 +1,14 @@
+let input = document.querySelector('#name-input')
+// console.log(input)
+let span = document.querySelector('#name-output')
+// console.log(span)
+
+input.addEventListener('input', onInputChange)
+
+function onInputChange(event) {
+  
+    // if (event.currentTarget.value !== '') {
+    //   span.textContent = 'Anonymous'  
+    // }
+    span.textContent = event.currentTarget.value;
+}
