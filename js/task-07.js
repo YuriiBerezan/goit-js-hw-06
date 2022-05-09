6406ae0c89600760db60e390760bbc5e11b07716
Yurii Berezan <67425022+YuriiBerezan@users.noreply.github.com>
2022-05-09 16:02:46 +0300

    add 7 task

---
+++ js/task-07.js
@@ -0,0 +1,10 @@
+const textEl = document.getElementById("text");
+
+const inputEl = document.getElementById("font-size-control");
+
+inputEl.addEventListener("input", event => {
+  const size = inputEl.value;
+  console.log(size);
+
+  textEl.style.fontSize = size + "px";
+});
