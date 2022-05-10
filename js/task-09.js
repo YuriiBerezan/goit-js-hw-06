a42e58772a0322b8826b090a7ceae0f162c9aa74
Yurii Berezan <67425022+YuriiBerezan@users.noreply.github.com>
2022-05-10 14:28:35 +0300

    add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const changeColorBtn = document.querySelector('.change-color')
+let changeColorText = document.querySelector('.color')
+// console.log(changeColorText)
+
+changeColorBtn.addEventListener('click', onChangeColor)
+
+function onChangeColor( )
+{
+  let colorRandom = document.body.style.backgroundColor = getRandomHexColor();
+  changeColorText.innerText = colorRandom;
+}
