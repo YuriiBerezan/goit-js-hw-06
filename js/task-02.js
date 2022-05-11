7ada3df597e56da5f25213b5db06806031632aef
Yurii Berezan <67425022+YuriiBerezan@users.noreply.github.com>
2022-05-11 15:33:42 +0300

    update task 2

--- js/task-02.js
+++ js/task-02.js
@@ -10,20 +10,31 @@ const ingredients = [
 const ulEl = document.querySelector('#ingredients');
 // console.log(ulEl)
 
-const ulAddLi = document.createElement('li');
+// const ulAddLi = document.createElement('li');
 
-ulAddLi.textContent = 'onion';
-ulAddLi.classList.add('item')
-// console.log(ulAddLi)
+// ulAddLi.textContent = 'onion';
+// ulAddLi.classList.add('item')
+// // console.log(ulAddLi)
 
-// const list = ingredients.map(option => {
+// // const list = ingredients.map(option => {
 
-// })
+// // })
 
-const list = ingredients.reduce((str, item) => str + `<li>${item}</li>`, '');
-// console.log(list)
-ulEl.innerHTML = list;
-// console.log(list)
-console.log(ulEl)
-// ulEl.append(list, ulAddLi);   
-ulEl.appendChild(ulAddLi)
+// const list = ingredients.reduce((str, item) => str + `<li>${item}</li>`, '');
+// // console.log(list)
+// ulEl.innerHTML = list;
+// // console.log(list)
+// console.log(ulEl)
+// // ulEl.append(list, ulAddLi);   
+// ulEl.appendChild(ulAddLi)
+
+const addListItems = (array) => {
+    const listArray = array.map(item => {
+        const listItem = document.createElement('li');
+        listItem.textContent = item;
+        return listItem;
+    })
+    ulEl.append(...listArray);
+}
+
+addListItems(ingredients);
